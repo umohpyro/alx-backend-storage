f66569896d0bd547b5278c252df3365df2dbe37a
Umoh Andem <andemumoh@gmail.com>
2023-05-10 22:09:21 +0000

    Task 5. Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,57 +1,36 @@
 #!/usr/bin/env python3
-
 """
-This module provides the class Cache
+web cache and tracker
 """
-
-from functools import wraps
-import redis
 import requests
-import requests_html
-import typing
+import redis
+from functools import wraps
+
+store = redis.Redis()
 
 
-def counter(method: typing.Callable):
-    """
-    Counts the calls to the input method
-    """
-
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
-    def count(self, url: str, *args, **kwargs) -> typing.Callable:
-        """
-        Caches the counts of visits to url
-        """
-        key = 'count:' + url
-        self._redis.incr(key)
-        self._redis.expire(key, 10)
-        return counter(url, *args, **kwargs)
-    return count
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
 
 
-class Cache:
-    """
-    This class queries a webpage and monitors the number of visits to it
-    """
-
-    def __init__(self):
-        """
-        Initialize class
-        """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    @counter
-    def get_page(self, url: str) -> str:
-        """
-        This function ses the requests module to obtain
-        the HTML content of a particular URL and returns it
-        """
-        response = requests.get(url)
-        return response.text
-
-
-if __name__ == '__main__':
-    url = 'http://slowwly.robertomurray.co.uk'
-    cache = Cache()
-    print(cache)
-    cache.get_page(url)
+@count_url_access
+def get_page(url: str) -> str:
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
